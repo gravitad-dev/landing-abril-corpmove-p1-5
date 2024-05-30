5b7051af93ab897e3abcfeab652397956b2f6796
silvak <silvak.jeg@gmail.com>
2024-05-30 12:19:57 -0400

    first commit -d

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='frameContainer'>
+      <iframe src="https://landing-april-corpmove-p1-5-next.vercel.app/" frameborder="0" ></iframe>
+    </div>
+  )
+}
+
+export default App
